8d66975f3c4aa40415571383575f20c76155330c
daniel-cao-byte <91414247+daniel-cao-byte@users.noreply.github.com>
2025-04-24 18:54:25 +0800

    feat: fix context address setting

--- framework/src/main/java/org/tron/core/services/http/RunOpServlet.java
+++ framework/src/main/java/org/tron/core/services/http/RunOpServlet.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.bouncycastle.util.encoders.Hex;
 import org.eclipse.jetty.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tron.common.runtime.InternalTransaction;
 import org.tron.common.runtime.vm.DataWord;
@@ -15,7 +16,12 @@ import org.tron.core.vm.JumpTable;
 import org.tron.core.vm.Operation;
 import org.tron.core.vm.OperationRegistry;
 import org.tron.core.vm.program.Program;
+import org.tron.core.vm.program.invoke.ProgramInvoke;
+import org.tron.core.vm.program.invoke.ProgramInvokeFactory;
+import org.tron.core.vm.program.invoke.ProgramInvokeImpl;
 import org.tron.core.vm.program.invoke.ProgramInvokeMockImpl;
+import org.tron.core.vm.repository.Repository;
+import org.tron.core.vm.repository.RepositoryImpl;
 import org.tron.protos.Protocol;
 
 import javax.servlet.http.HttpServletRequest;
@@ -82,10 +88,24 @@ public class RunOpServlet extends RateLimiterServlet {
 
     private void runOp(byte[] bytecodes, byte[] codeAddress, List<String> stackValues) throws ContractValidateException {
         for (int i = 0; i < round; i++) {
-            ProgramInvokeMockImpl invoke = new ProgramInvokeMockImpl(StoreFactory.getInstance(), bytecodes, codeAddress);
+            ProgramInvokeMockImpl invoke0 = new ProgramInvokeMockImpl(StoreFactory.getInstance(), bytecodes, codeAddress);
             Protocol.Transaction trx = Protocol.Transaction.getDefaultInstance();
             InternalTransaction interTrx =
-                    new InternalTransaction(trx, InternalTransaction.TrxType.TRX_UNKNOWN_TYPE);
+                new InternalTransaction(trx, InternalTransaction.TrxType.TRX_UNKNOWN_TYPE);
+
+            long vmStartInUs = System.nanoTime() / 1000;
+            Repository rootRepository = RepositoryImpl.createRoot(StoreFactory.getInstance());
+
+            ProgramInvoke invoke = ProgramInvokeFactory.createProgramInvoke(
+                new Program(bytecodes, codeAddress, invoke0, interTrx), new DataWord(codeAddress),
+                new DataWord(codeAddress),
+                DataWord.ZERO(),
+                DataWord.ZERO(),
+                DataWord.ZERO(),
+                0, new byte[0], rootRepository,
+                false,
+                false, vmStartInUs, vmStartInUs + 1_000_000_000L, 100_000_000L);
+
             Program program = new Program(bytecodes, codeAddress, invoke, interTrx);
             for (String value : stackValues) {
                 if (value.equals("randomAddress")) {
